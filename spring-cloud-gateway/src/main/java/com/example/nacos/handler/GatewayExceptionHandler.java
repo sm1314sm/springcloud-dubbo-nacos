366ea7af1a30b46aec58d5a4c200d73fec9402c8
gehx <1731571327@qq.com>
2021-01-19 11:32:13 +0800

    修改gateway

--- spring-cloud-gateway/src/main/java/com/example/nacos/handler/GatewayExceptionHandler.java
+++ spring-cloud-gateway/src/main/java/com/example/nacos/handler/GatewayExceptionHandler.java
@@ -1,10 +1,9 @@
 package com.example.nacos.handler;
 
 import com.alibaba.fastjson.JSON;
-import com.example.nacos.domain.R;
+import com.example.nacos.domain.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.support.NotFoundException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -14,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
 
 /**
@@ -21,7 +21,7 @@ import reactor.core.publisher.Mono;
  */
 @Order(-1)
 @Configuration
-public class GatewayExceptionHandler implements ErrorWebExceptionHandler {
+public class GatewayExceptionHandler implements WebExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GatewayExceptionHandler.class);
 
     @Override
@@ -50,7 +50,7 @@ public class GatewayExceptionHandler implements ErrorWebExceptionHandler {
 
         return response.writeWith(Mono.fromSupplier(() -> {
             DataBufferFactory bufferFactory = response.bufferFactory();
-            return bufferFactory.wrap(JSON.toJSONBytes(R.fail(msg)));
+            return bufferFactory.wrap(JSON.toJSONBytes(AjaxResult.error(msg)));
         }));
     }
 }
