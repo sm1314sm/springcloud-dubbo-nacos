e1bb9736fd553b741de1ba4c6c78cf611d542ea4
gehx <1731571327@qq.com>
2021-01-19 18:03:14 +0800

    添加springcloudadmin

--- spring-cloud-gateway/src/main/java/com/example/nacos/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/example/nacos/SpringCloudGatewayApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NacosApplication {
+public class SpringCloudGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NacosApplication.class, args);
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
     }
 }
